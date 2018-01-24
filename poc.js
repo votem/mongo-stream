0ab1ac4fd83dd90fe61d4f23c49983999d21b204
Andrew Van De Poel <andrewvdp@gmail.com>
2018-01-24 12:02:18 -0800

    ssl connections

--- poc.js
+++ poc.js
@@ -1,8 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
+const f = require('util').format;
 const BSON = require('bson')
 const bson = new BSON();
-const url = 'mongodb://localhost:27017';
+let url = process.env.MONGO_RS || 'mongodb://localhost:27017';
 const dbName = 'vrs';
 let resumeToken;
 
@@ -18,9 +19,35 @@ try {
 console.log('resumeToken', resumeToken);
 
 const collectionName = 'voter';
-MongoClient.connect(url, (err, client) => {
+
+let options = {};
+
+if (process.env.ROOT_FILE_PATH) {
+  const ca = fs.readFileSync(process.env.ROOT_FILE_PATH);
+  const cert = fs.readFileSync(process.env.KEY_FILE_PATH);
+  const key = fs.readFileSync(process.env.KEY_FILE_PATH);
+
+  options = {
+    ssl: true,
+    sslCA: ca,
+    sslKey: key,
+    sslCert: cert
+  };
+
+  const user = encodeURIComponent(process.env.MONGO_USER);
+  url = f('mongodb://%s@%s', user, process.env.MONGO_RS)
+}
+
+console.log('connecting to mongo...');
+console.log('url', url);
+MongoClient.connect(url, options, (err, client) => {
+  if (err)
+    console.error(err);
+
   const db = client.db(dbName);
   const stream = db.collection(collectionName).watch({ resumeAfter: resumeToken });
+
+  console.log('connected');
 
   stream.on('change', function(change) {
     console.log(change)
