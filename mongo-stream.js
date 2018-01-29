745c409438c643f3fccbcb1868cbbf5c14e0e785
Guillermo Valdez <guillermo.valdez@everyonecounts.com>
2018-01-28 17:06:07 -0800

    Fleshed out simple server with more replication commands

--- mongo-stream.js
+++ mongo-stream.js
@@ -6,168 +6,224 @@ const MongoClient = require('mongodb').MongoClient;
 
 
 class MongoStream {
-    constructor(db){
-        this.esClient =  new elasticsearch.Client({host: 'localhost:9200', apiVersion: '2.4'});
-        this.db = db;
+  constructor(db) {
+    this.esClient = new elasticsearch.Client({host: 'localhost:9200', apiVersion: '2.4'});
+    this.db = db;
+    this.changeStreams = {};
+  }
+
+  // constructs and returns a new MongoStream
+  static async init(url, options, dbName) {
+    const client = await MongoClient.connect(url, options);
+    const db = client.db(dbName);
+    const mongoStream = new MongoStream(db);
+
+    // add listeners to all collections in our database
+    const collections = await db.collections();
+    for (let i = 0; i < collections.length; i++) {
+      const collectionName = collections[i].collectionName;
+      if (collectionName !== 'voter') continue; // right now, we're only listening to the voter collection
+      mongoStream.addChangeStream(collectionName);
     }
 
-    static async init(url, options, dbName) {
-        const client = await MongoClient.connect(url, options);
-        const db = client.db(dbName);
-        const mongoStream = new MongoStream(db);
-        const collections = await db.collections();
-        for (let i = 0; i < collections.length; i++) {
-            const collectionName = collections[i].collectionName;
-            if (collectionName !== 'voter') continue;
-            console.log(collectionName);
-            const resumeToken = mongoStream.parseResumeToken(collectionName);
-            if (!resumeToken) await mongoStream.collectionDump(collectionName);
-            const change = db.collection(collectionName).watch({resumeAfter: resumeToken});
-            change.on('change', function (change) {
-                const b64String = bson.serialize(change._id).toString('base64');
-                fs.writeFileSync(`./resumeTokens/${collectionName}`, b64String, 'base64');
-                mongoStream.replicate(change);
-            });
-        }
+    return mongoStream;
+  }
 
-        return mongoStream;
+  // delete all docs in ES before dumping the new docs into it
+  // There's a better way to do this, I'm sure, but I'll figure it out later
+  async deleteESCollection(collectionName, limit) {
+    let allESDocks;
+    try {
+      // First get a count for all ES docs of the specified type
+      allESDocks = await this.esClient.count({
+        index: this.db.databaseName,
+        type: collectionName
+      });
+    }
+    catch(err) {
+      // if the count query failed, the index or type does not exist
+      allESDocks = {count: 0}
     }
 
-    async collectionDump(collectionName) {
-        console.log(`dumping from ${collectionName}`);
-        this.esClient.indices.delete({index: collectionName});
-        const count = await this.db.collection(collectionName).count();
-        console.log(count);
-        let bulkOpsDone = 0;
-
-        const limit = 100;
-        for (let i = 0; i < Math.ceil(count/limit); i++) {
-            const docPack = await this.db.collection(collectionName).find({}, {
-                limit: limit,
-                skip: i * limit
-            }).toArray();
-            const bulkOp = [];
-            for (let j = 0; j < docPack.length; j++) {
-                const _id = docPack[j]._id;
-                delete docPack[j]._id;
-                bulkOp.push({index: {_index: this.db.databaseName, _type: collectionName, _id: _id}});
-                bulkOp.push(docPack[j]);
-            }
-            bulkOpsDone += bulkOp.length/2;
-            await this.sendBulkrequest(bulkOp);
-            console.log(`${collectionName}: ${bulkOpsDone}/${count}`);
-        }
-
-        console.log('done');
-
-        return bulkOpsDone;
+    // loop through all existing esdocks in increments of bulksize, then delete them
+    let numDeleted = 0;
+    for (let i = 0; i < Math.ceil(allESDocks.count / limit); i++) {
+      const searchResponse = await this.esClient.search({
+        index: this.db.databaseName,
+        type: collectionName,
+        from: limit * i,
+        size: limit
+      });
+      const bulkDelete = [];
+      const dumpDocs = searchResponse.hits.hits;
+      for (let j = 0; j < dumpDocs.length; j++) {
+        bulkDelete.push({delete: {_index: this.db.databaseName, _type: collectionName, _id: dumpDocs[j]._id}})
+      }
+      numDeleted += bulkDelete.length;
+      console.log(`${collectionName} delete progress: ${numDeleted}/${allESDocks.count}`);
+      await this.sendBulkRequest(bulkDelete);
     }
 
-    sendBulkrequest(bulkOp) {
-        return this.esClient.bulk({
-            refresh: false,
-            body: bulkOp
-        }).then(resp => {
-            return;
-        }).catch(err => {
-            console.log(err);
-        })
+    return numDeleted;
+  }
+
+  // overwrites an entire elasticsearch collection with the current collection state in mongodb
+  async collectionDump(collectionName, limit = 100) {
+    console.log(`dumping from ${collectionName}`);
+
+    await this.deleteESCollection(collectionName, limit);
+
+    // count and replicate documents from mongo into elasticsearch
+    const count = await this.db.collection(collectionName).count();
+    let bulkOpsDone = 0;
+    for (let i = 0; i < Math.ceil(count / limit); i++) {
+      const docPack = await this.db.collection(collectionName).find({}, {
+        limit: limit,
+        skip: i * limit
+      }).toArray();
+      const bulkOp = [];
+      for (let j = 0; j < docPack.length; j++) {
+        const _id = docPack[j]._id;
+        delete docPack[j]._id;
+        bulkOp.push({index: {_index: this.db.databaseName, _type: collectionName, _id: _id}});
+        bulkOp.push(docPack[j]);
+      }
+      bulkOpsDone += bulkOp.length / 2;
+      await this.sendBulkRequest(bulkOp);
+      console.log(`${collectionName}s replicated: ${bulkOpsDone}/${count}`);
     }
 
-    parseResumeToken(collection) {
-        try {
-            const base64Buffer = fs.readFileSync(`./resumeTokens/${collection}`);
-            return bson.deserialize(base64Buffer);
-        } catch (err) {
-            console.log(err.message);
-            return null;
-        }
-    }
+    console.log('done');
+    return bulkOpsDone;
+  }
 
-    async replicate(change) {
-        console.log(`${change.documentKey._id.toString()} - ${change.ns.coll} ${change.operationType} BEGIN`);
-        await this[`${change.operationType}Doc`](change)
-          .catch(err => console.log(`${change.operationType} error`, err));
-        console.log(`${change.documentKey._id.toString()} - ${change.ns.coll} ${change.operationType} END`);
+  sendBulkRequest(bulkOp) {
+    return this.esClient.bulk({
+      refresh: false,
+      body: bulkOp
+    }).then(resp => {
+      return;
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  static parseResumeToken(collection) {
+    try {
+      const base64Buffer = fs.readFileSync(`./resumeTokens/${collection}`);
+      return bson.deserialize(base64Buffer);
+    } catch (err) {
+      return null;
     }
+  }
+
+  async addChangeStream(collectionName) {
+    const resumeToken = this.parseResumeToken(collectionName);
+    if (!resumeToken) await this.collectionDump(collectionName);
+    if (this.changeStreams[collectionName]) {
+      console.log('change stream already exists, removing...');
+      this.removeChangeStream(collectionName);
+    }
+    this.changeStreams[collectionName] = this.db.collection(collectionName).watch({resumeAfter: resumeToken});
+    const mongoStream = this; // I'm bad at scope, needed access to 'this' in the below callback
+    this.changeStreams[collectionName].on('change', function (change) {
+      const b64String = bson.serialize(change._id).toString('base64');
+      fs.writeFileSync(`./resumeTokens/${collectionName}`, b64String, 'base64');
+      mongoStream.replicate(change);
+    });
+
+    this.changeStreams[collectionName] = this.db.collection(collectionName).watch();
+  }
+
+  removeChangeStream(collectionName) {
+    this.changeStreams[collectionName].close();
+    this.changeStreams[collectionName] = null;
+  }
+
+  async replicate(change) {
+    console.log(`${change.documentKey._id.toString()} - ${change.ns.coll} ${change.operationType} BEGIN`);
+    await this[`${change.operationType}Doc`](change)
+      .catch(err => console.log(`${change.operationType} error`, err));
+    console.log(`${change.documentKey._id.toString()} - ${change.ns.coll} ${change.operationType} END`);
+  }
 
 // insert event format https://docs.mongodb.com/manual/reference/change-events/#insert-event
-    insertDoc(changeStreamObj) {
-        const esIndex = changeStreamObj.ns.db;
-        const esType = changeStreamObj.ns.coll;
-        const esId = changeStreamObj.fullDocument._id.toString(); // convert mongo ObjectId to string
-        delete changeStreamObj.fullDocument._id;
-        const esReadyDoc = changeStreamObj.fullDocument;
+  insertDoc(changeStreamObj) {
+    const esIndex = changeStreamObj.ns.db;
+    const esType = changeStreamObj.ns.coll;
+    const esId = changeStreamObj.fullDocument._id.toString(); // convert mongo ObjectId to string
+    delete changeStreamObj.fullDocument._id;
+    const esReadyDoc = changeStreamObj.fullDocument;
 
-        return this.esClient.create({
-            index: esIndex,
-            type: esType,
-            id: esId,
-            body: esReadyDoc
-        });
-    }
+    return this.esClient.create({
+      index: esIndex,
+      type: esType,
+      id: esId,
+      body: esReadyDoc
+    });
+  }
 
 // lookup doc in ES, apply changes, index doc
 // not the most efficient but until we need to optimize it
 // this is the most straightforward
-    updateDoc(changeStreamObj) {
-        const esIndex = changeStreamObj.ns.db;
-        const esType = changeStreamObj.ns.coll;
-        const esId = changeStreamObj.documentKey._id.toString(); // convert mongo ObjectId to string
-        const updatedFields = changeStreamObj.updateDescription.updatedFields;
-        const removedFields = changeStreamObj.updateDescription.removedFields;
+  updateDoc(changeStreamObj) {
+    const esIndex = changeStreamObj.ns.db;
+    const esType = changeStreamObj.ns.coll;
+    const esId = changeStreamObj.documentKey._id.toString(); // convert mongo ObjectId to string
+    const updatedFields = changeStreamObj.updateDescription.updatedFields;
+    const removedFields = changeStreamObj.updateDescription.removedFields;
 
-        return this.esClient.get({
-            index: esIndex,
-            type: esType,
-            id: esId
-        }).then(doc => {
-            const source = doc._source;
-            removedFields.forEach(field => {
-                delete source[field];
-            });
-            const esReadyDoc = Object.assign(source, updatedFields);
+    return this.esClient.get({
+      index: esIndex,
+      type: esType,
+      id: esId
+    }).then(doc => {
+      const source = doc._source;
+      removedFields.forEach(field => {
+        delete source[field];
+      });
+      const esReadyDoc = Object.assign(source, updatedFields);
 
-            return this.esClient.index({
-                index: esIndex,
-                type: esType,
-                id: esId,
-                body: esReadyDoc
-            });
-        });
-    }
+      return this.esClient.index({
+        index: esIndex,
+        type: esType,
+        id: esId,
+        body: esReadyDoc
+      });
+    });
+  }
 
-    replaceDoc(changeStreamObj) {
-        const esIndex = changeStreamObj.ns.db;
-        const esType = changeStreamObj.ns.coll;
-        const esId = changeStreamObj.fullDocument._id.toString(); // convert mongo ObjectId to string
-        delete changeStreamObj.fullDocument._id;
-        const esReadyDoc = changeStreamObj.fullDocument;
+  replaceDoc(changeStreamObj) {
+    const esIndex = changeStreamObj.ns.db;
+    const esType = changeStreamObj.ns.coll;
+    const esId = changeStreamObj.fullDocument._id.toString(); // convert mongo ObjectId to string
+    delete changeStreamObj.fullDocument._id;
+    const esReadyDoc = changeStreamObj.fullDocument;
 
-        return this.esClient.index({
-            index: esIndex,
-            type: esType,
-            id: esId,
-            body: esReadyDoc
-        });
-    }
+    return this.esClient.index({
+      index: esIndex,
+      type: esType,
+      id: esId,
+      body: esReadyDoc
+    });
+  }
 
-    deleteDoc(changeStreamObj) {
-        const esIndex = changeStreamObj.ns.db;
-        const esType = changeStreamObj.ns.coll;
-        const esId = changeStreamObj.documentKey._id.toString(); // convert mongo ObjectId to string
+  deleteDoc(changeStreamObj) {
+    const esIndex = changeStreamObj.ns.db;
+    const esType = changeStreamObj.ns.coll;
+    const esId = changeStreamObj.documentKey._id.toString(); // convert mongo ObjectId to string
 
-        return this.esClient.delete({
-            index: esIndex,
-            type: esType,
-            id: esId
-        });
-    }
+    return this.esClient.delete({
+      index: esIndex,
+      type: esType,
+      id: esId
+    });
+  }
 
-    invalidateDoc(changeStreamObj) {
-        console.log('invalidate change received. The watched collection has been dropped or renamed. Stream closing...');
-        // do something to handle a stream closing I guess...
-    }
+  invalidateDoc(changeStreamObj) {
+    console.log('invalidate change received. The watched collection has been dropped or renamed. Stream closing...');
+    // do something to handle a stream closing I guess...
+  }
 
 }
 
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8421;
 
-// const poc = require('./poc');
 const MongoStream = require('./mongo-stream');
 let mongoStream;
 
@@ -10,10 +9,39 @@ app.get('/', (request, response) => {
     response.send('Hello from Express!')
 });
 
+// triggers a manual collection dump for the specified collection
 app.get('/dump/:collection', (request, response) => {
     console.log(`dumping collection ${request.params.collection} to ES`);
     mongoStream.collectionDump(request.params.collection);
     response.send(`dumping collection ${request.params.collection} to ES`);
+});
+
+// returns the status of all change streams currently running
+app.get('/monitor', (request, response) => {
+    const changeStreams = Object.keys(mongoStream.changeStreams);
+    const responseBody = {};
+    for (let i = 0; i < changeStreams.length; i++) {
+        if (mongoStream.changeStreams[changeStreams[i]]) {
+            responseBody[changeStreams[i]] = 'Listening'
+        }
+        else {
+            responseBody[changeStreams[i]] = 'Not Listening'
+        }
+    }
+    response.send(JSON.stringify(responseBody, null, 2));
+});
+
+// starts listening to the change stream of the specified collection
+app.get('/start/:collection', (request, response) => {
+    mongoStream.addChangeStream(request.params.collection).then(() => {
+      response.send(`${request.params.collection} change stream added`);
+    });
+});
+
+// stops listening to the change stream of the specified collection
+app.get('/stop/:collection', (request, response) => {
+    mongoStream.removeChangeStream(request.params.collection);
+    response.send(`${request.params.collection} change stream removed`);
 });
 
 app.listen(port, (err) => {
